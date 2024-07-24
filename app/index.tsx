59e3b549e12d42d50228502e1797edccdf6dd0c0
martinromario55 <martinromario55@gmail.com>
2024-07-24 22:18:27 +0300

    Apply custom fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,21 @@
+import { useFonts } from 'expo-font'
 import { Text, View } from 'react-native'
 
 export default function Index() {
+  useFonts({
+    'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('./../assets/fonts/Outfit-Medium.ttf'),
+    outfit: require('./../assets/fonts/Outfit-Regular.ttf'),
+  })
   return (
     <View
       style={{
         flex: 1,
       }}
     >
-      <Text>Hello World!</Text>
+      <Text style={{ fontSize: 30, fontFamily: 'outfit-bold' }}>
+        Hello World!
+      </Text>
     </View>
   )
 }
